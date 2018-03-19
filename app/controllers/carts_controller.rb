e491453933c7870fe13cf643b2dfb33fd3276482
Ninh <ninhle.9984@gmail.com>
2018-03-19 14:35:56 +0700

    Finish-cart

---
+++ app/controllers/carts_controller.rb
@@ -0,0 +1,24 @@
+class CartsController < ApplicationController
+  attr_reader :cart
+
+  before_action :find_cart
+
+  def show; end
+
+  def destroy
+    cart.destroy
+    session[:cart_id] = nil
+    redirect_to root_url
+  end
+
+  private
+
+  def find_cart
+    @cart = Cart.find_by id: params[:id]
+    current_cart = Cart.find_by id: session[:cart_id]
+
+    return if cart && cart.current_cart?(current_cart)
+    redirect_to root_url
+    flash[:danger] = t ".invalid"
+  end
+end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,23 @@
 class Cart < ApplicationRecord
   has_many :line_items, dependent: :destroy
+
+  def current_cart? cart
+    self == cart
+  end
+
+  def add_product product
+    product_id = product.id
+    line_item = line_items.find_by product_id: product_id
+
+    if (current_item = line_item)
+      current_item.quantity += 1
+    else
+      current_item = line_items.build product_id: product_id
+    end
+    current_item
+  end
+
+  def total_price
+    line_items.to_a.sum &:total_price
+  end
 end
